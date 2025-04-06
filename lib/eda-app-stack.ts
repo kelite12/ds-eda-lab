af6014b109d4f4f739e279ed3c4574b0119dd914
kelite12 <20108870@mail.wit.ie>
2025-04-06 19:24:30 +0100

    Signup resource

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -5,25 +5,53 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3n from "aws-cdk-lib/aws-s3-notifications";
 import * as events from "aws-cdk-lib/aws-lambda-event-sources";
 import * as sqs from "aws-cdk-lib/aws-sqs";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as iam from "aws-cdk-lib/aws-iam";
-
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EDAAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // S3 Bucket
     const imagesBucket = new s3.Bucket(this, "images", {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       publicReadAccess: false,
     });
 
-    // Output
-    
+    // SQS Queue
+    const queue = new sqs.Queue(this, "img-created-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(5),
+    });
+
+    // Lambda Function
+    const processImageFn = new lambdanode.NodejsFunction(
+      this,
+      "ProcessImageFn",
+      {
+        runtime: lambda.Runtime.NODEJS_22_X,
+        entry: `${__dirname}/../lambdas/processImage.ts`,
+        timeout: cdk.Duration.seconds(15),
+        memorySize: 128,
+      }
+    );
+
+    // S3 --> SQS Event Notification
+    imagesBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SqsDestination(queue)
+    );
+
+    // SQS --> Lambda Trigger (Batch Processing)
+    const newImageEventSource = new events.SqsEventSource(queue, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(5),
+    });
+    processImageFn.addEventSource(newImageEventSource);
+
+    // Permissions
+    imagesBucket.grantRead(processImageFn);
+
+    // Output (仅保留一个 bucketName 定义)
     new cdk.CfnOutput(this, "bucketName", {
       value: imagesBucket.bucketName,
     });
